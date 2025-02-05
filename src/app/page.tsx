fa1bff21b4175c3151a13f205928bca6637d10f0
Abhuday Lath <abhudaylath@gmail.com>
2025-02-05 17:36:05 +0530

    finished the before after section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import BeforeAfter from "@/components/before-after";
 import { Container } from "@/components/container";
 import { Header } from "@/components/header";
 import Hero from "@/components/hero";
+//import LogoClouds from "@/components/logo-clouds";
 
 export default function Home() {
   return (
     <Container>
       <Header></Header>
       <Hero/>
+      {/*<LogoClouds/>*/}
+      <BeforeAfter/>
     </Container>
   );
 }
